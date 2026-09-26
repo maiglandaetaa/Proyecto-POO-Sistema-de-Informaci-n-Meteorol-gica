e2988cbf5929d1850553beb6455df8d1db48695f
feñaña <fernanda.lopez2601@alumnos.ubiobio.cl>
2026-09-26 17:23:43 -0300

    Clase interfaz de usuario (test)

---
+++ srs/InterfazUsuario.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+
+public class InterfazUsuario {
+    private Scanner sc = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        System.out.println("Holi :D...");
+    }
+
+
+    private void menuPrincipal() {
+        System.out.println("SYSTEMA DE INFORMACIÓN METEOROLÓGICA");
+        System.out.println("_____________________________________________");
+        System.out.println("\n1. Crear región");
+        System.out.println("2. Crear comuna");
+        System.out.println("3. Crear estación");
+        System.out.println("4. Instalar sensor");
+        System.out.println("5. Registrar medición");
+        System.out.println("6. Generar listados");
+        System.out.println("7. Salir");
+        System.out.print("Opción: ");
+        int opcion = sc.nextInt();
+
+
+        switch (opcion) {
+            case 1 -> { }
+        }
+    }
+
+
+    private void crearRegion() {
+
+
+    }
+
+
+    private void crearComuna() {
+
+
+    }
+
+
+    private void crearEstacionMeteorologica() {
+
+
+    }
+
+
+    private void instalarSensor() {
+
+
+    }
+
+
+    private void registrarMedicion() {
+
+
+    }
+
+
+    private void menuListados() {
+
+
+    }
+
+
+    private void listarRegiones() {
+
+
+    }
+
+
+    private void listarComunas() {
+
+
+    }
+
+
+    private void listarEstaciones() {
+
+
+    }
+
+
+    private void listarSensores() {
+
+
+    }
+
+
+    private void listarMediciones() {
+
+
+    }
+
+}
